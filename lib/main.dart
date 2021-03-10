cb8d19bd3d618b42eed75b38c0e4680b3b44f0d7
Mohamed Reda <mohamed@wrangleapp.io>
2021-03-10 19:06:55 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Hi'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int page = 1;
+  List<String> items = [
+    'item 1',
+    'item 2',
+    'item 1',
+    'item 2',
+    'item 1',
+    'item 2',
+    'item 1',
+    'item 2',
+    'item 1',
+    'item 2',
+  ];
+  bool isLoading = false;
+
+  Future _loadData() async {
+    // perform fetching data delay
+    await new Future.delayed(new Duration(seconds: 2));
+
+    print("load more");
+    // update data and loading status
+    setState(() {
+      items.addAll(['item 1']);
+      print('items: ' + items.toString());
+      isLoading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: NotificationListener<ScrollNotification>(
+              onNotification: (ScrollNotification scrollInfo) {
+                if (!isLoading &&
+                    scrollInfo.metrics.pixels ==
+                        scrollInfo.metrics.maxScrollExtent) {
+                  _loadData();
+                  // start loading data
+                  setState(() {
+                    isLoading = true;
+                  });
+                }
+                return;
+              },
+              child: ListView.builder(
+                itemCount: items.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text('${items[index]}'),
+                  );
+                },
+              ),
+            ),
+          ),
+          Container(
+            height: isLoading ? 50.0 : 0,
+            color: Colors.transparent,
+            child: Center(
+              child: new CircularProgressIndicator(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
